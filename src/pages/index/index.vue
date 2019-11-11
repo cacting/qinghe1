58c5f7ceca421df6a799e327c44ee059a8f4b2ed
cacting <lt1477602761@163.com>
2019-11-11 21:23:18 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+</style>
